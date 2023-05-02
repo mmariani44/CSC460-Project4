0b5f7bc63145406e7c2a9443b1b853bce4b97624
Arnav Popat <arnavpopat@Arnavs-MacBook-Pro-2.local>
2023-05-02 00:34:40 -0700

    fixed queries

--- queries/Queries.java
+++ queries/Queries.java
@@ -5,57 +5,45 @@ public class Queries {
 
 			
 """
-SELECT c.firstName, c.lastName, 
-SUM(DATEDIFF(d, b.startDate, b.endDate) * r.roomRate) + 
-SUM(a.amenityCost) AS TotalCost
-FROM Hotel.Client c
-INNER JOIN Hotel.Booking b ON c.clientId = b.clientId
-INNER JOIN Hotel.Room r ON b.roomId = r.roomId AND b.hotelId = r.hotelId
+SELECT SUM(rb.roomRate) + COALESCE(SUM(a.amenityCost),0) AS totalBill
+FROM HotelBooking b
+INNER JOIN HotelRoom r ON b.roomId = r.roomId AND b.hotelId = r.hotelId
+INNER JOIN HotelClient c ON b.clientId = c.clientId
+INNER JOIN (
+    SELECT roomId, hotelId, roomRate
+    FROM HotelRoom
+) rb ON b.roomId = rb.roomId AND b.hotelId = rb.hotelId
 LEFT JOIN (
-  SELECT amenityId, hotelId, SUM(amenityCost) AS amenityCost
-  FROM (
-    SELECT a.amenityId, a.hotelId, a.amenityName, a.ratingId, 
-           a.costPerUse * COUNT(*) AS amenityCost
-    FROM Hotel.Amenity a
-    LEFT JOIN Hotel.Rating r ON a.ratingId = r.ratingId
-    INNER JOIN Hotel.Booking b ON a.hotelId = b.hotelId
-    WHERE b.clientId = %s AND r.rating < 3
-    GROUP BY a.amenityId, a.hotelId, a.costPerUse
-  ) AS subquery
-  GROUP BY amenityId, hotelId
-) AS a ON b.hotelId = a.hotelId
-WHERE c.clientId = %s
-GROUP BY c.firstName, c.lastName;			
+    SELECT amenityId, hotelId, amenityCost
+    FROM HotelAmenity
+    WHERE isPaid = 0
+) a ON b.hotelId = a.hotelId AND r.roomId = a.roomId
+WHERE b.clientId = %s
+GROUP BY b.clientId;
+			
 """;
 	
 	public static final String query2 = // parameter(s): date 
 			
 """
-SELECT c.firstName, c.lastName, r.roomId, 
-CASE WHEN c.isStudent = 1 THEN 'Student'
-     WHEN c.membershipType = 'Gold' THEN 'Gold Member'
-     WHEN c.membershipType = 'Silver' THEN 'Silver Member'
-     ELSE 'Regular' END AS CustomerCategory
-FROM Hotel.Client c
-INNER JOIN Hotel.Booking b ON c.clientId = b.clientId
-INNER JOIN Hotel.Room r ON b.roomId = r.roomId AND b.hotelId = r.hotelId
-WHERE b.startDate <= TO_DATE('%s','YYYY-MM-DD') AND b.endDate >= TO_DATE('%s','YYYY-MM-DD')
-ORDER BY r.roomId, CustomerCategory;		
+SELECT c.firstName, c.lastName, r.roomId, CASE WHEN c.membershipType IS NULL THEN 'Non-Member' ELSE c.membershipType END AS category
+FROM HotelClient c
+INNER JOIN HotelBooking b ON c.clientId = b.clientId
+INNER JOIN HotelRoom r ON b.roomId = r.roomId AND b.hotelId = r.hotelId
+WHERE TO_DATE('%s', 'YYYY-MM-DD') BETWEEN b.startDate AND b.endDate
+ORDER BY r.roomId, category;
+		
 """;
 
 	public static final String query3 = // parameters(s): startdate
 
 """
-SELECT c.firstName, c.lastName, r.roomId, 
-CASE WHEN c.isStudent = 1 THEN 'Student'
-     WHEN c.membershipType = 'Gold' THEN 'Gold Member'
-     WHEN c.membershipType = 'Silver' THEN 'Silver Member'
-     ELSE 'Regular' END AS CustomerCategory
-FROM Hotel.Client c
-INNER JOIN Hotel.Booking b ON c.clientId = b.clientId
-INNER JOIN Hotel.Room r ON b.roomId = r.roomId AND b.hotelId = r.hotelId
-WHERE b.startDate <= TO_DATE('%s','YYYY-MM-DD') AND b.endDate >= TO_DATE('%s','YYYY-MM-DD')
-ORDER BY r.roomId, CustomerCategory;
+SELECT e.firstName, e.lastName, s.dateStart, s.dateEnd, s.dateEnd - s.dateStart AS hoursWorked
+FROM HotelEmployee e
+INNER JOIN HotelShift s ON e.employeeId = s.employeeId
+WHERE s.dateStart >= TO_DATE('%s', 'YYYY-MM-DD') AND s.dateStart < TO_DATE('%s', 'YYYY-MM-DD') + 7
+ORDER BY e.lastName, e.firstName, s.dateStart;
+
 """;
 
 	
@@ -63,26 +51,26 @@ ORDER BY r.roomId, CustomerCategory;
 
 			
 """
-SELECT a.amenityName, AVG(r.rating) AS AverageRating
-FROM Hotel.Amenity a
-INNER JOIN Hotel.Rating r ON a.amenityId = r.amenityId AND r.dateStart >= TO_DATE('%s','YYYY-MM-DD') AND r.dateStart <= TO_DATE('%s','YYYY-MM-DD')
+SELECT a.amenityName, AVG(r.rating) AS avgRating
+FROM HotelAmenity a
+INNER JOIN HotelRating r ON a.amenityId = r.amenityId
+WHERE r.dateRecorded BETWEEN TO_DATE('%s', 'YYYY-MM-DD')  AND TO_DATE('%s', 'YYYY-MM-DD') 
 GROUP BY a.amenityName
-ORDER BY AverageRating DESC;			
+ORDER BY avgRating DESC;
+		
 """;
 	
 	// Print the top 5 amenities by the average rating over a specified date range for a specific hotel.
 	// Allow the user to input the hotel ID, start date, and end date.
 	public static final String query5 =
 """
-SELECT a.amenityName, AVG(r.rating) AS avg_rating
-FROM Hotel.Amenity a
-INNER JOIN Hotel.Rating r ON a.amenityId = r.amenityId
-INNER JOIN Hotel.Booking b ON r.clientId = b.clientId AND r.hotelId = b.hotelId
-WHERE b.hotelId = %s
-AND b.startDate >= %s
-AND b.endDate <= %s
-GROUP BY a.amenityName
-ORDER BY AVG(r.rating) DESC;
+SELECT c.firstName, c.lastName, r.roomId, b.startDate, b.endDate
+FROM HotelClient c
+INNER JOIN HotelBooking b ON c.clientId = b.clientId
+INNER JOIN HotelRoom r ON b.roomId = r.roomId AND b.hotelId = r.hotelId
+WHERE c.isStudent = %s
+ORDER BY r.roomId;
+
 """;	
 			
 }
